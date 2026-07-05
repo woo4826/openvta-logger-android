b1b806cf4502d6f33739d66dffd4f7f76bba7d80
woo4826 <woo4826@gmail.com>
2026-07-06 02:04:45 +0900

    chore: bump android logger to 0.0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,8 +39,8 @@ android {
         applicationId = "dev.openvta.logger"
         minSdk = 29
         targetSdk = 35
-        versionCode = 3
-        versionName = "0.0.3"
+        versionCode = 4
+        versionName = "0.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
